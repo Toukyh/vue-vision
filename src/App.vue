21abba67f9c94046e441530ad6a4bbaf0ab16bf6
Toky Yves <toky.r@nosytech.fr>
2024-09-05 13:34:48 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-between items-center gap-2">
+    <button class="p-3 bg-red-600 rounded-md text-white" @click="store.decrement()">-</button>
+    <div class="text-white font-semibold text-xl">
+      count :
+      {{ store.count }}
+    </div>
+    <button class="p-3 bg-green-600 rounded-md text-white" @click="store.increment()">+</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { useCounterStore } from './stores/counter'
+
+const store = useCounterStore()
+</script>
